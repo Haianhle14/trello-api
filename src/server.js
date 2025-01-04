ff00043002089bacebf070ea8e46a4796841aa48
Le Hai Anh <haianh102003@gmail.com>
2025-01-04 16:45:46 +0700

    Init Base Code & Connect MongoDB

---
+++ src/server.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
+const START_SERVER = () => {
+  const app = express()
+
+  app.get('/', async(req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, Back-end Server is running successfully at Host:${env.APP_HOST} and Port: ${env.APP_PORT}`)
+  })
+
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB Atlas!'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
+
